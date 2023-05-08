bd9d6190baa6e327a7ddc4fdfb196332f61431e5
Joaquin Bravo Contreras <jackbravo@gmail.com>
2023-05-07 20:48:47 -0600

    Update the script to take source and destination folders from command arguments
    Prompt for ChatGPT for the changes:
    > make the root directory a parameter you get from the command line, as well as the destination directory for all the files

--- import_blog.py
+++ import_blog.py
@@ -1,15 +1,19 @@
 import os
 import shutil
+import argparse
 
-# Specify the path to the directory containing the "pages" folder
-root_directory = "/path/to/root/directory"
+# Set up command line argument parser
+parser = argparse.ArgumentParser(description="Move and rename index.md files in the pages directory.")
+parser.add_argument("root_directory", type=str, help="Path to the root directory.")
+parser.add_argument("destination_directory", type=str, help="Path to the destination directory.")
+args = parser.parse_args()
 
 # Define a function to move index.md files and rename them
-def move_index_files(root_directory):
+def move_index_files(root_directory, destination_directory):
     # Navigate to the "pages" folder
     pages_directory = os.path.join(root_directory, "pages")
     os.chdir(pages_directory)
-    
+
     # Loop through all directories in the "pages" folder
     for directory in os.listdir():
         # Check if the current item is a directory
@@ -19,10 +23,11 @@ def move_index_files(root_directory):
             if os.path.isfile(index_file):
                 # Determine the new filename based on the directory name
                 new_filename = f"{directory}.md"
-                
-                # Move and rename the file
-                destination_file = os.path.join(root_directory, new_filename)
-                shutil.move(index_file, destination_file)
 
-# Call the function
-move_index_files(root_directory)
+                # Move and rename the file to the destination directory
+                source_file = os.path.join(pages_directory, index_file)
+                destination_file = os.path.join(destination_directory, new_filename)
+                shutil.move(source_file, destination_file)
+
+# Call the function with command line arguments
+move_index_files(args.root_directory, args.destination_directory)
